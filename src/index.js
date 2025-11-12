bc65e1f6ae3d3d26858e699e285e0357dd872b5e
SABI9666 <sabincn676@gmail.com>
2025-11-13 00:14:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 const serverless = require('serverless-http');
 
 // Import the Express app from server.js
+// Note: We are now loading 'server.js' instead of having all the code here.
 const app = require('./server'); 
 
 // Export the handler for AWS Lambda
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app);
